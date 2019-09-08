5f3a11bb8c3465c94ff0da15e914ea3e54cd3062
neha1908 <neha@bizongo.in>
2019-09-08 21:07:37 +0530

    Adds changes for animated carousel

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  width: 100%;
-  overflow: scroll;
+  max-width: 100%;
+  overflow: hidden;
   display: flex;
   align-items: center;
   justify-content: space-between;
   box-sizing: border-box;
   position: relative;
+`;
+const SlidesWrapper = styled.ul`
+  width: ${props => props.width}%;
+  transform: translateX(-${props => props.translate}%);
+  display: block;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  transition: transform .5s;
+  transition-timing-function: cubic-bezier(.645,.045,.355,1);
+  will-change: transform;
 `;
 const PreviousButton = styled.div`
   z-index: 1;
@@ -26,40 +37,40 @@ const NextButton = styled.div`
   padding: 5px;
   cursor: pointer;
 `;
-const LeftSlide = styled.div`
-  width: 20%;
-  margin: 0 20px;
-  overflow: hidden;
-  transition: all 1s;
-  
-  img {
-    transform: translateX(-65%);
-  }
-`;
-const RightSlide = styled.div`
-  width: 20%;
-  margin: 0 20px;
-  overflow: hidden;
-  transition: all 1s;
-  
-  img {
-    height: 100%;
-  }
-`;
-const MainSlide = styled.div`
-  width: 60%;
+const MainSlide = styled.li`
+  width: ${props => props.width - 6}%;
   padding: 0 20px;
-  transition: all 1s;
+  position: relative;
+  display: inline-block;
+  box-sizing: border-box;
+  height: 0;
+  margin: 0;
+  list-style-type: none;
+  z-index: ${props => props.active ? 1 : 0};
   
   img {
     width: 100%;
     display: inline-block;
   }
   
-  :hover {
-    transform: scale(1.2);
-    transition: all 1s;
-  }
+  ${props => props.active && `
+    :hover {
+      transform: scale(1.2);
+      transition: all 1s;
+      z-index: 1;
+    }
+  `}
+  
+  ${props => props.isLeft && `
+    transform-origin: top right;
+    transform: skewY(5deg);
+  `}
+  
+  ${props => props.isRight && `
+    transform-origin: top left;
+    transform: skewY(-5deg);
+  `}
+  
 `;
 
 const Carousel = (props) => {
@@ -70,29 +81,46 @@ const Carousel = (props) => {
   }
 
   const getLeftSlide = () => {
-    return props.children[activeIndex > 0 ? activeIndex - 1 : props.children.length - 1];
+    return activeIndex > 0 && activeIndex - 1;
   }
 
   const getRightSlide = () => {
-    return props.children[activeIndex < props.children.length - 1 ? activeIndex + 1 : 0];
+    return activeIndex < props.children.length - 1 && activeIndex + 1;
   }
 
   const goToNext = () => {
-    setActiveIndex(activeIndex < props.children.length - 1 ? activeIndex + 1 : 0)
+    setActiveIndex(activeIndex < props.children.length - 2 ? activeIndex + 1 : activeIndex)
   }
 
   const goToPrev = () => {
-    setActiveIndex(activeIndex > 0 ? activeIndex - 1 : props.children.length - 1)
+    setActiveIndex(activeIndex > 0 ? activeIndex - 1 : activeIndex)
+  }
+
+  const getTranslateValue = () => {
+    return (11 * activeIndex) + 3 * (activeIndex - 1);
   }
 
   if(!props.children) return null;
 
   return (
     <Wrapper>
+      <div>
+        <SlidesWrapper width={props.children.length * 100} translate={getTranslateValue()}>
+          {props.children.map((child, index) => {
+            return (
+              <MainSlide
+                width={100/props.children.length}
+                active={activeIndex === index}
+                isLeft={index === getLeftSlide()}
+                isRight={index === getRightSlide()}
+              >
+                {child}
+              </MainSlide>
+            )
+          })}
+        </SlidesWrapper>
+      </div>
       <PreviousButton onClick={goToPrev}>Prev</PreviousButton>
-      <LeftSlide>{getLeftSlide()}</LeftSlide>
-      <MainSlide>{getMainSlide()}</MainSlide>
-      <RightSlide>{getRightSlide()}</RightSlide>
       <NextButton onClick={goToNext}>Next</NextButton>
     </Wrapper>
   )
